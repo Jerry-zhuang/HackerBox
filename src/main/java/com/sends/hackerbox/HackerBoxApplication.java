1a6242c682f18a00873aefe4bb2f36e1d80adb5a
a1ertx5s <924292308@qq.com>
2025-02-14 21:31:54 +0800

    初始化项目

---
+++ src/main/java/com/sends/hackerbox/HackerBoxApplication.java
@@ -0,0 +1,13 @@
+package com.sends.hackerbox;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HackerBoxApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HackerBoxApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sends/hackerbox/HackerBoxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sends.hackerbox;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HackerBoxApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
